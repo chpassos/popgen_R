2cd973afe2f5c8fefca51b180ee3fd16456f0cba
Carlos Passos <crlshnrqpassos@gmail.com>
2021-06-02 18:02:39 -0300

    Create admixing_chromosomes.R

---
+++ admixing_chromosomes/admixing_chromosomes.R
@@ -0,0 +1,51 @@
+# Admixing chromosomes in R
+library(tidyverse)
+
+
+# Building chromosomes
+df <- data.frame("chromosome" = c(rep(c(1), 1000), rep(c(2), 1000)), 
+                 "position" = c(1:1000, 1:1000),
+                 "type" = c(rep(c("A"), 1000), rep(c("B"), 1000)))
+
+
+types <- c("A", "B")
+ancestry_choice <- sample(types, size = 1, prob = c(0.5, 0.5))
+
+
+output <- data.frame(matrix(ncol=3, nrow=1000))
+for(i in 1:1000){
+  
+  recombination <- sample(c(TRUE, FALSE), size = 1, prob = c(0.05, 1 - 0.05))
+  
+  if(recombination == FALSE){
+    chr_pos <- filter(df, position == i & type %in% ancestry_choice)
+    output[i, ] <- chr_pos
+    output[i,]
+    
+  } else{
+    ancestry_choice <- types[!types %in%ancestry_choice]
+    chr_pos <- filter(df, position == i & type %in% ancestry_choice)
+    output[i, ] <- chr_pos
+    output[i,]
+  }
+  
+}
+
+output %>%
+  select("position" = X2, "type" = X3) %>% 
+  mutate("chromosome" = rep(1, 1000)) %>%
+  ggplot(aes(chromosome, position, fill = type)) +
+  geom_tile()
+
+
+
+
+
+## EXAMPLE -- 
+filter(df, type %in% ancestry_choice)
+filter(df, !(type %in% ancestry_choice))
+output <- data.frame(matrix(ncol=3, nrow=1000))
+i <- 467
+chr_pos <- filter(df, position == i & type == "B")
+output[i, ] <- chr_pos
+output[i,]
